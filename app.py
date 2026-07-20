beb5eaf55fc9e6c190f4e573821169f27e47fa78
DEKA12345 <siteshine2@gmail.com>
2026-07-20 13:20:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 st.title("카운터 앱")
 ct = 0
-if st.button("증가"):
-    ct += 1
-st.markdown(f"## 현재 숫자 : `{ct}`")
+while 1:
+
+    if st.button("증가"):
+        ct += 1
+    st.markdown(f"## 현재 숫자 : `{ct}`")
