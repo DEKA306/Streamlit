2b919cba79047f0629895a190a5ba900793f363b
DEKA12345 <siteshine2@gmail.com>
2026-07-20 10:33:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.markdown("# ai 챗 봇 만들기")
 st.markdown("---")
-st.markdown("##질문을 하시면 ai가답할거옝/ㅛ ")
+st.markdown("## 질문을 하시면 ai가답할거옝/ㅛ ")
 st.markdown("* **제작 언어**: 'python'")
 st.markdown("* **프레임워크**: 'streamlut'")
 st.markdown("### '최근 대화'")
