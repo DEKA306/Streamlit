a5915eed9682d0b7cb61b92a9cf267e9557a22be
DEKA12345 <siteshine2@gmail.com>
2026-07-20 10:33:15 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
-st.title("나의웹앱 ")
-st.header("Hello World!")
-st.subheader("입력안내")
-st.write("질문 : ")
+st.markdown("# ai 챗 봇 만들기")
+st.markdown("---")
+st.markdown("##질문을 하시면 ai가답할거옝/ㅛ ")
+st.markdown("* **제작 언어**: 'python'")
+st.markdown("* **프레임워크**: 'streamlut'")
+st.markdown("### '최근 대화'")
+st.markdown("~~질문을자유롭게 입력하세요.~~ ")
