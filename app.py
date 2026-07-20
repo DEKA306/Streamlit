399ee6ec3f4c134a7c9a1f2932b4b9ede072421e
DEKA12345 <siteshine2@gmail.com>
2026-07-20 13:05:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ with col2:
     top_color = st.select_slider("핏(Fit)", options=["슬림", "레귤러", "오버핏"])
 
 st.header("디테일 추가")
-tab1, tab2 = st.tabs(["신발"], ["악세서리"])
+tab1, tab2 = st.tabs(["신발", "악세서리"])
 with tab1:
     st.write("오늘의 발걸음을 책임질 신발:")
     shoes = st.selectbox("신발 선택", ["스니커즈", "운동화", "구두", "슬리퍼"])
