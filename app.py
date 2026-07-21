f00c9e7d0d7a0e5c962765d36d65209f79003483
DEKA12345 <siteshine2@gmail.com>
2026-07-21 12:22:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,38 +10,42 @@ local_storage = LocalStorage()
 
 st.title("AI Task Manager")
 
-# 저장된 작업 가져오기
-tasks = local_storage.getItem("tasks")
-
-if tasks is None:
-    tasks = []
-
-# 입력
-task = st.text_input(
-    "앞으로 해야 할 일을 입력하세요",
-    placeholder="여기에 입력해주세요. "
-)
-
-# 추가
-if st.button("추가"):
-    if task:
-        tasks.append(task)
-        local_storage.setItem("tasks", tasks)
-        st.success("저장되었습니다.")
-        st.rerun()
-    else:
-        st.warning("할 일을 입력해주세요.")
-
-st.subheader("해야 할 일")
-
-for i, t in enumerate(tasks):
-    col1, col2 = st.columns([4, 1])
-
-    with col1:
-        st.write(f"{i+1}. {t}")
-
-    with col2:
-        if st.button("제거", key=f"remove_{i}"):
-            tasks.pop(i)
+def pg1():
+    # 저장된 작업 가져오기
+    tasks = local_storage.getItem("tasks")
+    
+    if tasks is None:
+        tasks = []
+    
+    # 입력
+    task = st.text_input(
+        "앞으로 해야 할 일을 입력하세요",
+        placeholder="여기에 입력해주세요. "
+    )
+    
+    # 추가
+    if st.button("추가"):
+        if task:
+            tasks.append(task)
             local_storage.setItem("tasks", tasks)
+            st.success("저장되었습니다.")
             st.rerun()
+        else:
+            st.warning("할 일을 입력해주세요.")
+    
+    st.subheader("해야 할 일")
+    
+    for i, t in enumerate(tasks):
+        col1, col2 = st.columns([4, 1])
+    
+        with col1:
+            st.write(f"{i+1}. {t}")
+    
+        with col2:
+            if st.button("제거", key=f"remove_{i}"):
+                tasks.pop(i)
+                local_storage.setItem("tasks", tasks)
+                st.rerun()
+pg = st.navigation([
+    st.Page(pg1, title="오늘의 다짐")], position="top")
+pg.run()
